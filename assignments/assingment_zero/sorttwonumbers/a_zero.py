3774100672db56edb48907efc3d37c2fc5b8f617
Daniel <dfishbein@mavs.coloradomesa.edu>
2024-01-26 21:30:20 +0000

    added some more comments

--- assignments/assingment_zero/sorttwonumbers/a_zero.py
+++ assignments/assingment_zero/sorttwonumbers/a_zero.py
@@ -3,6 +3,13 @@ from typing import Tuple
 
 
 def print_nums(num1: int, num2: int) -> None:
+    """Prints the numbers after they have been
+    sorted from the switch_nums function
+
+    Args:
+        num1 (int): first number that was entered
+        num2 (int): second number that was entered
+    """
     print(f"{num1} {num2}")
 
 
@@ -24,12 +31,22 @@ def switch_nums(num1: int, num2: int) -> Tuple[int, int]:
 
 
 def get_nums() -> Tuple[int, int]:
+    """Function to get the numbers that are
+    entered
+
+    Returns:
+        Tuple[int, int]: Changes the numbers 
+        from strings to ints
+    """
     line = sys.stdin.readline()
     num1, num2 = line.split()
     return int(num1), int(num2)
 
 
 def main() -> None:
+    """Main function that does all the calling of the
+    separate functions
+    """
     in_num1, in_num2 = get_nums()
     num1, num2 = switch_nums(in_num1, in_num2)
     print_nums(num1, num2)
